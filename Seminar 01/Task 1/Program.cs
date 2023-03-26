a2c91ce58d3add536b971bd7b80c8263eb03b1df
Vladimir Petrov <vlizupegaq@gmail.com>
2023-03-26 13:58:24 +0300

    task 1

---
+++ Seminar 01/Task 1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача: найти квадрат числа
+
+// Считываем данные из консоли
+string? inLine = Console.ReadLine();
+
+// Проверяем, чтобы данные были не пустыми
+if (inLine != null)
+{   // Парсим введенное число
+    int inNumber = int.Parse(inLine);
+    // Находим введенное число
+    int outNumber = inNumber*inNumber;
+    // Выводим данные в консоль
+    Console.WriteLine(outNumber);
+}
