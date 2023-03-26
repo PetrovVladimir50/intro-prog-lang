4ce8ffe4a659c07c276f58266baad3d9592a3d47
Vladimir Petrov <vlizupegaq@gmail.com>
2023-03-26 16:29:38 +0300

    task 4

--- Seminar 01/Task 2/Program.cs
+++ Seminar 01/Task 2/Program.cs
@@ -1,13 +1,17 @@
-﻿Console.Write("Enter first number: ");
+﻿// Ввод первого числа
+Console.Write("Enter first number: ");
 string? number1Line = Console.ReadLine();
+// Ввод второго числа
 Console.Write("Enter second number: ");
 string? number2Line = Console.ReadLine();
-
+// Проверка на пустое значение
 if (number1Line!=null && number2Line!=null)
 {
+    // Парсим
     int number1 = int.Parse(number1Line);
     int number2 = int.Parse(number2Line);
 
+    // Проверка
     if (number1>number2)
     {
         Console.Write("max: ");
---
+++ Seminar 01/Task 4/Program.cs
@@ -0,0 +1,36 @@
+﻿// Ввод значений
+Console.Write("Enter first number: ");
+string? num1Line = Console.ReadLine();
+Console.Write("Enter second number: ");
+string? num2Line = Console.ReadLine();
+Console.Write("Enter third number: ");
+string? num3Line = Console.ReadLine();
+
+// Проверка на пустое занчение
+if (num1Line!=null && num2Line!=null && num3Line!=null)
+{
+    // Парсим
+    int num1 = int.Parse(num1Line);
+    int num2 = int.Parse(num2Line);
+    int num3 = int.Parse(num3Line);
+
+    int maxnum = 0; // Чтобы maxnum был в области видимости всех циклов
+
+    // Проверка
+    if (num1>num2)
+    {
+        maxnum = num1;
+    }
+    else
+    {
+        maxnum = num2;
+    }
+
+    if (num3>maxnum)
+    {
+        maxnum = num3;
+    }
+    // Вывод
+    Console.Write("Max number: ");
+    Console.Write(maxnum);
+}
