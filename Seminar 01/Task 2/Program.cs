40eabd0edb22503925a1b81ec5e89e7405ce9357
Vladimir Petrov <vlizupegaq@gmail.com>
2023-03-26 16:00:20 +0300

    task 2

---
+++ Seminar 01/Task 2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Enter first number: ");
+string? number1Line = Console.ReadLine();
+Console.Write("Enter second number: ");
+string? number2Line = Console.ReadLine();
+
+if (number1Line!=null && number2Line!=null)
+{
+    int number1 = int.Parse(number1Line);
+    int number2 = int.Parse(number2Line);
+
+    if (number1>number2)
+    {
+        Console.Write("max: ");
+        Console.WriteLine(number1);
+    }
+    else
+    {
+        Console.Write("max: ");
+        Console.WriteLine(number2);
+    }
+}
