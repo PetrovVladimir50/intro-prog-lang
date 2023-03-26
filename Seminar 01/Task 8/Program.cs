97cdffc8e165104162fe6fd1abdd6a2cda8b84e8
Vladimir Petrov <vlizupegaq@gmail.com>
2023-03-26 17:50:39 +0300

    task 8, HW coplete

---
+++ Seminar 01/Task 8/Program.cs
@@ -0,0 +1,28 @@
+﻿// На вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число N: ");
+//Считываем данные с консоли
+string? numLine = Console.ReadLine();
+
+// Проверка на null
+if(numLine!=null)
+{
+    int endNum = int.Parse(numLine);
+
+    int lstNum = 1;
+
+    string res = string.Empty;
+
+    // Цикл
+    while (lstNum<=endNum)
+    {
+        int remainder = lstNum % 2;
+        if (remainder == 0)
+        {
+            res = res + lstNum + " ";
+        }
+        lstNum++;
+    }
+    Console.Write("Четные числа от 1 до N: ");
+    Console.Write(res);
+}
