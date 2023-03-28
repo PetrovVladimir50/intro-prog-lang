dabce43a2d0640fa3ff96f1d837647098daf7860
Vladimir Petrov <vlizupegaq@gmail.com>
2023-03-28 22:33:51 +0300

    task 9 11 12

---
+++ Seminar 02/Task 11/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №11: Напишите программу, которая выводит случайное трехзначное число 
+// и удаляет вторую цифру этого числа.
+
+// // Вариант 1
+// int number = new Random().Next(100,1000);
+// Console.WriteLine(number);
+
+// int firstDigit = number/100;
+// int lastDigit = number% 10;
+// int newNumber = firstDigit*10 + lastDigit;
+
+// Console.WriteLine(newNumber);
+
+// Вариант 2
+int number = new Random().Next(100,1000);
+Console.WriteLine(number);
+char[] digits = number.ToString().ToCharArray();
+char[] result = {digits[0], digits[2]};
+Console.WriteLine(result);
---
+++ Seminar 02/Task 9/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача №9: 
+// Напишите программу, которая выводит случайное число и отрезка 10-99,
+// показывает наибольшую цифру этого числа.
+
+// int number = new Random().Next(10,99);
+// Console.WriteLine(number);
+
+System.Random numSintezator = new System.Random();
+
+// Вариант 1
+int rndNum = numSintezator.Next(10,100);
+Console.WriteLine(rndNum);
+
+short firstDigit = (short)(rndNum/10); // Преобразование int в short
+short secondDigit = (short)(rndNum % 10);
+
+Console.Write("Наибольшая цифра: ");
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else
+{
+    Console.WriteLine(secondDigit);
+}
+// Вариант 2
+char[] digits = numSintezator.Next(10,100).ToString().ToCharArray();
+Console.WriteLine(digits);
+int firstNum = (int) (digits[0]-48);
+int secondNum = (int) (digits[1]-48);
+// Тернарный оператор
+int result = (firstNum > secondNum)? firstNum:secondNum;
+Console.WriteLine(result);
