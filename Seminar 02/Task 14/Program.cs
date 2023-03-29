86ff94f3e75199897ad7dd9f4fbdf7834776dc6b
Vladimir Petrov <vlizupegaq@gmail.com>
2023-03-29 17:01:06 +0300

    task 14 16

---
+++ Seminar 02/Task 14/Program.cs
@@ -0,0 +1,13 @@
+﻿// Здача №14: Напишите программу, которая на вход принмает число
+// и проверяет кратно ли оно однвременно и 7 и 23.
+
+int num1 = int.Parse(Console.ReadLine()??"0");
+
+if (num1 % 7 == 0 && num1 % 23 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
---
+++ Seminar 02/Task 16/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача №16: Напишите программу, которая принмает на вход два числа
+// и проверят, является ли одно число квадратом другого.
+
+int num1 = int.Parse(Console.ReadLine()??"0");
+int num2 = int.Parse(Console.ReadLine()??"0");
+
+void Test (int a, int b)
+{
+    if (a == b*b)
+    {
+        Console.WriteLine(a + " квадарат " + b);
+    }
+    else
+    {
+        Console.WriteLine(a + " не квадарат " + b);
+    }
+}
+
+Test(num1, num2);
+Test(num2, num1);
